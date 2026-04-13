4d4adba6702ca79e343cc39132bcdb5123e9b76a
1234 <a1234@1234ui-MacBookAir.local>
2026-04-13 13:15:33 +0900

    update

--- src/main/java/com/nhnacademy/springbootmvc/controller/HomeController.java
+++ src/main/java/com/nhnacademy/springbootmvc/controller/HomeController.java
@@ -2,12 +2,18 @@ package com.nhnacademy.springbootmvc.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
 
-    @GetMapping("/")
-    public String index() {
+//    @GetMapping("/")
+//    public String index() {
+//        return "index";
+//    }
+
+    @RequestMapping("/")
+    public String home(){
         return "index";
     }
 }
--- src/main/java/com/nhnacademy/springbootmvc/controller/UserController.java
+++ src/main/java/com/nhnacademy/springbootmvc/controller/UserController.java
@@ -3,9 +3,11 @@ package com.nhnacademy.springbootmvc.controller;
 import com.nhnacademy.springbootmvc.domain.User;
 import com.nhnacademy.springbootmvc.repository.UserRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
+import java.util.Map;
 
 @Controller
 public class UserController {
@@ -16,9 +18,10 @@ public class UserController {
     }
 
     @GetMapping("/user")
-    public String getUsers(/* TODO 1: 적절한 모델 type 넣기 */) {
+    public String getUsers(Model model) {
         List<User> users = userRepository.getUsers();
         //TODO 2: user 목록을 모델의 attribute 추가
+        model.addAttribute("users",users);
         return "users";
     }
 }
