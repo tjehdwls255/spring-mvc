0180fa24bf8b3c2b626ded6db3c2eaec102ef25c
1234 <a1234@1234ui-MacBookAir.local>
2026-04-13 14:46:50 +0900

    update

--- src/main/java/com/nhnacademy/springbootmvc/controller/UserController.java
+++ src/main/java/com/nhnacademy/springbootmvc/controller/UserController.java
@@ -6,10 +6,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Repository;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,16 +19,13 @@ public class UserController {
         this.userRepository = userRepository;
     }
 
-
     // TODO: 아래 요청을 처리하는 컨트롤러 메서드를 완성
 
     /* TODO 1
      * GET /users
      * 모든 저장된 멤버들을 출력
-     *
+     */
 
-
-    /* TODO 1-1 */
     @GetMapping("/user")
     public String getUsers(Model model) {
         List<User> users = userRepository.getUsers();
@@ -39,29 +33,45 @@ public class UserController {
         return "users";
     }
 
-    /** TODO 2
+    /**
+     * TODO 2
      * GET /user/{id}
      * id를 URL path variable 로 받아서 해당하는 유저를 조회
      */
 
     /* TODO 2-1 */
     @GetMapping("/user/{id}")
-    public String getUser(Model model,@PathVariable("id")/* TODO 2-2 */String id) {
+    public String getUser(Model model, @PathVariable("id") String id) {
         User user = userRepository.getUser(id);
         model.addAttribute("user", user);
         return "user";
     }
 
-    /** TODO 3
+    /**
+     * TODO 3
      * GET /user?id={id}
      * id를 request parameter 로 받아서 해당하는 유저를 조회
      */
 
     /* TODO 3-1 */
     @GetMapping("/user")
-    public String getUserByName(Model model,@RequestParam("id")/* TODO 3-2 */String id) {
+    public String getUserByName(Model model, @RequestParam("id") String id) {
         User user = userRepository.getUser(id);
         model.addAttribute("user", user);
         return "user";
     }
+
+    @GetMapping("/agent")
+    public String getUserAgent(Model model, @RequestHeader(name = "User-Agent") String userAgent) {
+        if (userAgent == "Android") {
+            model.addAttribute("agent", "Android");
+        } else if (userAgent.equals("iOS")) {
+            model.addAttribute("agent", "iOS");
+        } else {
+            model.addAttribute("agent", "PC");
+        }
+        return "user";
+    }
+
+
 }
