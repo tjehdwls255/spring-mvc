455afaf52c0165dfd06eb9624d8b8b4e65adb27b
1234 <a1234@1234ui-MacBookAir.local>
2026-04-14 16:52:58 +0900

    first

---
+++ src/main/java/com/nhnacademy/springbootmvc/intercepter/ProcessRImeInterceptor.java
@@ -0,0 +1,8 @@
+package com.nhnacademy.springbootmvc.intercepter;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+
+public class ProcessRImeInterceptor implements HandlerInterceptor {
+
+}
+
